55919f92f561adcdc98f6d9c2dd1880e70d1716b
Daffa Hilmy Fadhlurrohman <55197547+DaffaHilmyF@users.noreply.github.com>
2021-10-31 20:55:30 +0700

    add cors for api

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const MongoConn = require('./Utils/MongoDB/mongo-singleton.js')
 const MessageResponse = require('./Utils/Response/response.js')
 const API_V1 = require('./Router/API-V1/route_v1.js')
 const bodyParser = require('body-parser')
-
+const cors = require('cors')
 
 const server = express()
 const PORT = process.env.PORT || 5000
@@ -14,6 +14,7 @@ const dbConn = new MongoConn(process.env.NODE_ENV)
 server.use(morgan('myFormat'))
 server.use(bodyParser.json())
 server.use(bodyParser.urlencoded({extended: true}))
+server.use(cors())
 server.use('/', API_V1)
 
 server.get('/', (req, res)=>{
